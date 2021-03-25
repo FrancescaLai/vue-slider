c7c0574050dfe4d0d0407eba54747c196fb5b62f
FrancescaLai <francescalai1690@gmail.com>
2021-03-25 16:18:18 +0100

    vuejs 1 step (img added)  ✓

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var app = new Vue(
+  {
+    el: "#app",
+    data: {
+      images: [
+        {
+          titolo: "lago",
+          img: "img/lago.jpeg"
+        },
+        {
+          titolo: "mare",
+          img: "img/lago.jpeg"
+        },
+        {
+          titolo: "montagna",
+          img: "img/lago.jpeg"
+        }
+      ],
+      position: 0
+    },
+  }
+);
